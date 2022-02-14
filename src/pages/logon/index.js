1f6e855f5d026b74151847d7c06f894b38ba723c
Rafael Santos Freire <contato@rafascripts.site>
2022-02-13 23:45:04 -0300

    p

--- src/pages/logon/index.js
+++ src/pages/logon/index.js
@@ -2,8 +2,29 @@ import React from 'react';
 
 export default function logon(){
     return(
-        <div>
-            <h1>teste</h1>
+        <div className="">
+            <section className="">
+
+                <form onSubmit={}>
+                    <h1>Faça seu Logon</h1>
+
+                    <input
+                        placeholder="Sua ID"
+                        value={}
+                        onChange={}
+                    />
+
+                    <input
+                        placeholder="Sua Key"
+                        value={}
+                        onChange={}
+                    />
+
+
+                    <button className="button" type="submit">Entrar</button>
+                </form>
+
+            </section>
         </div>
     );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-
 import logon from './pages/logon';
 
 export default function Routes(){
