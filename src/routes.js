f5678711adeb546d5915d4e21870dc087e7bf471
RafaScripts <rafaelw@rafascripts.live>
2022-02-13 23:41:25 -0300

    p

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+import logon from './pages/logon';
+
+export default function Routes(){
+    return(
+        <BrowserRouter>
+            <Route path={'/'} component={logon} />
+        </BrowserRouter>
+    );
+}
